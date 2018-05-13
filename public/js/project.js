60c14525337cdf2f7c79c18a4b6381079288a358
keb18 <spliff.star@gmail.com>
2018-05-13 19:36:55 +0100

    Added dashboard route and creation of new project. Included the header and footer. Added bootstrap.

---
+++ public/js/project.js
@@ -0,0 +1,7 @@
+
+let addProject = document.getElementById("addProject");
+
+addProject.addEventListener("click", function(){
+    // document.getElementById("addProject").innerHTML = "Hello World";
+    document.location.href = '/dashboard/new';
+});
