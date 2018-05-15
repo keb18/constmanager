07d0c24207097886b72b870e357b7915d5408ec8
keb18 <spliff.star@gmail.com>
2018-05-15 01:43:05 +0100

    Added the projects link to the user schema

--- models/user.js
+++ models/user.js
@@ -2,14 +2,11 @@ const mongoose = require('mongoose');
 
 // Setup the user schema
 let userSchema = new mongoose.Schema({
-    userName: { type: String, unique: true },
+    username: { type: String, unique: true },
+    password: { type: String, unique: true },
     userJoined: { type: Date, default: Date.now },
-    company: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Company"
-        }
-    ]
+    company: [{type: mongoose.Schema.Types.ObjectId, ref: "Company"}],
+    projects: [{type: mongoose.Schema.Types.ObjectId, ref: "Project"}]
 });
 
 module.exports = mongoose.model('User', userSchema);
