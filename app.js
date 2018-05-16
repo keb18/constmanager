4deda769568ec918231465ab50d92224cd2d0c02
keb18 <spliff.star@gmail.com>
2018-05-16 20:20:25 +0100

    Routes refactored

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ const express = require('express'),
     LocalStrategy = require('passport-local');
 
 // Set-up module imports for the mongoose schemas
-const Project = require('./models/project'),
+let Project = require('./models/project'),
     Company = require('./models/company'),
     User = require('./models/user');
+
+// Require the external routes
+let companyRoutes = require('./routes/company'),
+    adminRoutes = require('./routes/admin'),
+    indexRoutes = require('./routes/index');
 
 // Passport configuration
 app.use(require('express-session')({
@@ -37,229 +42,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // use routes without the *.ejs extension
 app.set('view engine', 'ejs');
 
-// Landing page
-app.get('/', function (req, res) {
-    res.render('landing');
-});
 
-
-// =================================================================
-// ========================== ADMIN ===============================
-// ADMIN DASHBOARD
-app.get('/adminDashboard', isLoggedIn, disableCache, function (req, res) {
-    // Get all projects from db
-    Company.find({}, function (err, allCompanies) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('admin/adminDashboard', { companies: allCompanies });
-        }
-    });
-});
-
-
-// CREATE
-// ADMIN CREATE NEW COMPANY
-app.post('/adminDashboard', isLoggedIn, disableCache, function (req, res) {
-    // get data from form and add to projects array
-    let newCompany = {
-        companyName: req.body.companyName
-    }
-    // Create a new project and add to the db
-    Company.create(newCompany, function (err, newlyCreatedCompany) {
-        if (err) {
-            console.log(err);
-        } else {
-            // redirect back to the dashboard
-            res.redirect('/adminDashboard')
-        }
-    });
-});
-
-
-// ADMIN NEW COMPANY CREATION PAGE
-app.get('/adminDashboard/new', isLoggedIn, disableCache, function (req, res) {
-    res.render('admin/newCompany');
-});
-
-
-// ADMIN SHOW MORE INFO ABOUT ONE COMPANY
-app.get('/adminDashboard/company/:id', isLoggedIn, disableCache, function (req, res) {
-    // find project with provided id
-    Company.findById(req.params.id, function (err, foundCompany) {
-        if (err) {
-            console.log(err);
-        } else {
-            // render the project template with the specified id
-            res.render('admin/showCompany', { company: foundCompany });
-        }
-    });
-});
-
-
-// =================================================================
-// ========================== COMPANY ==============================
-// INDEX
-// COMPANY DASHBOARD
-app.get('/dashboard/:companyId', isLoggedIn, disableCache, function (req, res) {
-    Company.findById(req.params.companyId).populate('projects').exec(function (err, foundCompany) {
-        if (err) {
-            console.log(err);
-        } else {
-            // console.log(foundCompany);
-            // render the project template with the specified id
-            // res.send("This is the company dashboard page");
-            res.render('company/dashboard', {
-                projects: foundCompany.projects,
-                company: foundCompany
-            });
-        }
-    });
-});
-
-
-// NEW
-// NEW PROJECT CREATION PAGE
-app.get('/dashboard/:companyId/project/new', isLoggedIn, disableCache,function (req, res) {
-    Company.findById(req.params.companyId, function (err, foundCompany) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('project/newProject', { company: foundCompany });
-        }
-    });
-});
-
-
-// CREATE
-// CREATE NEW PROJECT
-app.post('/dashboard/:companyId/project', isLoggedIn, disableCache, function (req, res) {
-    // lookup company using ID
-    Company.findById(req.params.companyId, function (err, company) {
-        if (err) {
-            console.log(err);
-            res.redirect("/dashboard/" + company._id);
-        } else {
-            // Create a new project and add to the db
-            Project.create(req.body.project, function (err, newlyCreatedProject) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    company.projects.push(newlyCreatedProject);
-                    company.save();
-                    // redirect back to the dashboard
-                    res.redirect("/dashboard/" + company._id);
-                }
-            });
-        }
-    });
-});
-
-
-// SHOW
-// SHOW MORE INFO ABOUT ONE PROJECT
-app.get('/dashboard/:companyId/project/:projectId', isLoggedIn, disableCache, function (req, res) {
-    // find project with provided id
-    Project.findById(req.params.projectId).populate('projects').exec(function (err, foundProject) {
-        if (err) {
-            console.log(err);
-        } else {
-            Company.findById(req.params.companyId, function (err, foundCompany) {
-                if (err) {
-                    console.log(err);
-                    res.redirect("/dashboard/" + company._id);
-                } else {
-                    // render the project template with the specified id
-                    res.render('project/showProject', {
-                        project: foundProject,
-                        company: foundCompany
-                    });
-                }
-            });
-        };
-    });
-});
-
-// ==================================================
-// Authorisation routes
-// Show the register form
-app.get('/register', function (req, res) {
-    res.render('register');
-});
-
-// Handle register logic
-app.post('/register', function (req, res) {
-    let newUser = new User({ username: req.body.username });
-    User.register(newUser, req.body.password, function (err, user) {
-        if (err) {
-            console.log(err);
-            return res.render('register');
-        }
-
-        // Populate the user fields
-        user.firstName = req.body.firstName;
-        user.lastName = req.body.lastName;
-        user.email = req.body.email;
-        user.accountType = "owner";
-        // doc.visits.$inc();  == increment a value
-
-        // Create a new project and add to the db
-        Company.create({ companyName: req.body.companyName }, function (err, newlyCreatedCompany) {
-            if (err) {
-                console.log(err);
-            } else {
-                // Associate company with user
-                newlyCreatedCompany.users.push(user);
-                newlyCreatedCompany.save();
-                // Associate user with the company
-                user.company.push(newlyCreatedCompany);
-                user.save();
-                passport.authenticate('local')(req, res, function () {
-                    res.redirect('/dashboard/' + newlyCreatedCompany._id);
-                });
-            }
-        });
-    });
-});
-
-// Show the login form
-app.get('/login', function (req, res) {
-    res.render('login');
-});
-
-// Handle login logic
-app.post('/login', function (req, res, next) {
-    passport.authenticate('local', function (err, user, info) {
-        if (err) { return next(err); }
-        if (!user) { return res.redirect('/login'); }
-        req.logIn(user, function (err) {
-            if (err) { return next(err); }
-            return res.redirect('/dashboard/' + user.company);
-        });
-    })(req, res, next);
-});
-
-// Handle logout logic
-app.get('/logout', function(req, res){
-    req.logout();
-    res.redirect('/');
-});
-
-
-
-function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) {
-        next();
-    } else {
-        // req.flash("error", "You need to be logged in to do that."); // is handled in the /login
-        res.redirect("/login");
-    }
-}
-
-function disableCache(req, res, next) {
-    res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
-    next();
-}
+// Use the route files
+app.use('/dashboard', companyRoutes);
+app.use('/adminDashboard', adminRoutes);
+app.use(indexRoutes);
 
 
 // Express to listen for requests (start server)
@@ -269,5 +56,3 @@ const port = 3000;
 app.listen(port, hostname, function () {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-// http://127.0.0.1:3000/dashboard/5afb81de9254e75ec4878acb
--- middleware/index.js
+++ middleware/index.js
@@ -1,6 +1,7 @@
-var Campground = require("../models/campground"),
-    Comment = require("../models/comment");
-
+// Set-up module imports for the mongoose schemas
+let Project = require('../models/project'),
+  Company = require('../models/company'),
+  User = require('../models/user');
 
 // MIDDLEWARE
 var middlewareObject = {};
@@ -11,8 +12,8 @@ middlewareObject.isLoggedIn = function (req, res, next) {
     if (req.isAuthenticated()) {
         next();
     } else {
-        req.flash("error", "You need to be logged in to do that."); // is handled in the /login
-        res.redirect("/login");
+        // req.flash("error", "You need to be logged in to do that."); // is handled in the /login
+        res.redirect('/login');
     }
 }
 
@@ -25,57 +26,57 @@ middlewareObject.disableCache = function (req, res, next) {
     next();
 }
 
-// CHECK CAMPGROUND OWNERSHIP
-middlewareObject.checkCampOwnership = function (req, res, next) {
-    if (req.isAuthenticated()) {
-        // req.params.id gets it from the route which calls this function
-        Campground.findById(req.params.id, function (err, foundCampground) {
-            if (err) {
-                req.flash("error", "Campground was not found."); // is handled in the "back"
-                res.redirect("back");
-            } else {
-                // does user own the campground?
-                // req.user._id is available from app.js by passing current
-                // user to all routes
-                if (foundCampground.author.id.equals(req.user._id)) { // mongoose method
-                    // continue executing the code
-                    next();
-                } else {
-                    req.flash("error", "You can't modify campgrounds created by others."); // is handled in the /login
-                    res.redirect("back");
-                }
-            }
-        });
-    } else {
-        req.flash("error", "You need to be logged in to do that."); // is handled in the "back"
-        res.redirect("back");
-    }
-}
+// // CHECK CAMPGROUND OWNERSHIP
+// middlewareObject.checkCampOwnership = function (req, res, next) {
+//     if (req.isAuthenticated()) {
+//         // req.params.id gets it from the route which calls this function
+//         Campground.findById(req.params.id, function (err, foundCampground) {
+//             if (err) {
+//                 req.flash("error", "Campground was not found."); // is handled in the "back"
+//                 res.redirect("back");
+//             } else {
+//                 // does user own the campground?
+//                 // req.user._id is available from app.js by passing current
+//                 // user to all routes
+//                 if (foundCampground.author.id.equals(req.user._id)) { // mongoose method
+//                     // continue executing the code
+//                     next();
+//                 } else {
+//                     req.flash("error", "You can't modify campgrounds created by others."); // is handled in the /login
+//                     res.redirect("back");
+//                 }
+//             }
+//         });
+//     } else {
+//         req.flash("error", "You need to be logged in to do that."); // is handled in the "back"
+//         res.redirect("back");
+//     }
+// }
 
-// CHECK COMMENT OWNERSHIP
-middlewareObject.checkCommentOwnership = function(req, res, next) {
-    if (req.isAuthenticated()) {
-        // req.params.id gets it from the route which calls this function
-        Comment.findById(req.params.comment_id, function (err, foundComment) {
-            if (err) {
-                res.redirect("back");
-            } else {
-                // does user own the comment?
-                // req.user._id is available from app.js by passing current
-                // user to all routes
-                if (foundComment.author.id.equals(req.user._id)) { // mongoose method
-                    // continue executing the code
-                    next();
-                } else {
-                    req.flash("error", "You need to be logged in to do that."); // is handled in the "back"
-                    res.redirect("back");
-                }
-            }
-        });
-    } else {
-        res.redirect("back");
-    }
-}
+// // CHECK COMMENT OWNERSHIP
+// middlewareObject.checkCommentOwnership = function(req, res, next) {
+//     if (req.isAuthenticated()) {
+//         // req.params.id gets it from the route which calls this function
+//         Comment.findById(req.params.comment_id, function (err, foundComment) {
+//             if (err) {
+//                 res.redirect("back");
+//             } else {
+//                 // does user own the comment?
+//                 // req.user._id is available from app.js by passing current
+//                 // user to all routes
+//                 if (foundComment.author.id.equals(req.user._id)) { // mongoose method
+//                     // continue executing the code
+//                     next();
+//                 } else {
+//                     req.flash("error", "You need to be logged in to do that."); // is handled in the "back"
+//                     res.redirect("back");
+//                 }
+//             }
+//         });
+//     } else {
+//         res.redirect("back");
+//     }
+// }
 
 // EXPORT TO app.js
 module.exports = middlewareObject
